7f34b69b9fc5cbbc50257c1a90f17742163bfb90
LiterOfMilk <113127276+LiterOfMilk@users.noreply.github.com>
2023-03-17 09:13:57 -0700

    Added a restart button

--- CreateWithCode/Prototype5C/Assets/Scripts/GameManager.cs
+++ CreateWithCode/Prototype5C/Assets/Scripts/GameManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     private int score;
     private float spawnRate = 1.0f;
     public bool isGameActive;
@@ -47,7 +50,13 @@ public class GameManager : MonoBehaviour
     
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
